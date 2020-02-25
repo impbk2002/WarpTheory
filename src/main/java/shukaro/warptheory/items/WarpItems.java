2cef755969a839288ce184bcc11e8f74ff54b537
Prometheus0000000 <prometheus0000000@gmail.com>
2020-02-25 14:13:11 -0500

    Fix the stuff I didn't know I needed so it builds

--- src/main/java/shukaro/warptheory/items/WarpItems.java
+++ src/main/java/shukaro/warptheory/items/WarpItems.java
@@ -4,18 +4,21 @@ import cpw.mods.fml.common.registry.GameRegistry;
 
 public class WarpItems
 {
-    public static ItemCleanser itemCleanser;
+    public static ItemCleanserMinor itemCleanserMinor;
+	public static ItemCleanser itemCleanser;
     public static ItemAmulet itemAmulet;
     public static ItemSomething itemSomething;
     public static ItemPaper itemPaper;
 
     public static void initItems()
     {
-        itemCleanser = new ItemCleanser();
+        itemCleanserMinor = new ItemCleanserMinor();
+		itemCleanser = new ItemCleanser();
         itemAmulet = new ItemAmulet();
         itemSomething = new ItemSomething();
         itemPaper = new ItemPaper();
-        GameRegistry.registerItem(itemCleanser, itemCleanser.getUnlocalizedName());
+        GameRegistry.registerItem(itemCleanserMinor, itemCleanserMinor.getUnlocalizedName());
+		GameRegistry.registerItem(itemCleanser, itemCleanser.getUnlocalizedName());
         GameRegistry.registerItem(itemAmulet, itemAmulet.getUnlocalizedName());
         GameRegistry.registerItem(itemSomething, itemSomething.getUnlocalizedName());
         GameRegistry.registerItem(itemPaper, itemPaper.getUnlocalizedName());
