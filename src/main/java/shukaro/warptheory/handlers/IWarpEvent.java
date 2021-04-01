ff331112024eac54169fa90302a3f2c536a6b920
impbk2002 <impbk2002@naver.com>
2021-04-01 10:33:41 +0900

    Update IWarpEvent.java

--- src/main/java/shukaro/warptheory/handlers/IWarpEvent.java
+++ src/main/java/shukaro/warptheory/handlers/IWarpEvent.java
@@ -13,13 +13,12 @@ public abstract class IWarpEvent
 
     public final int getCost() { return (int)Math.ceil(getSeverity() / (double)10); }
 
-	public boolean canDo(EntityPlayer player) {
-		boolean flag = true;
-		flag = !player.isPotionActive(potionWarpWardID) || (WarpHandler.getUnavoidableCount(player) > 0);
-		WarpHandler.addUnavoidableCount(player, -1);
-		flag &= !(wuss||player.capabilities.isCreativeMode);
-		return flag;
-	}
+    public boolean canDo(EntityPlayer player)
+    {
+        boolean flag = !player.isPotionActive(potionWarpWardID) || (WarpHandler.getUnavoidableCount(player) > 0) && !(wuss||player.capabilities.isCreativeMode);
+        WarpHandler.addUnavoidableCount(player, -1);
+        return flag;
+    }
 
     public abstract boolean doEvent(World world, EntityPlayer player);
 }
